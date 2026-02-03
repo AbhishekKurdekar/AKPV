f4b62dcdc43a838764df900e8dd8b9eaa8b10b4d
AbhishekKurdekar <95276063+AbhishekKurdekar@users.noreply.github.com>
2026-02-03 19:25:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+function unlock() {
+  const pass = document.getElementById("password").value;
+  if (pass === "myvalentine") {   // CHANGE PASSWORD HERE
+    document.getElementById("lockScreen").style.display = "none";
+    document.getElementById("content").style.display = "block";
+  } else {
+    document.getElementById("error").innerText = "Wrong password 💔";
+  }
+}
 
+function moveNo() {
+  const no = document.getElementById("no");
+  const x = Math.random() * 250 - 125;
+  const y = Math.random() * 120;
+  no.style.transform = `translate(${x}px, ${y}px)`;
+}
+
+function yesClick() {
+  document.body.innerHTML = `
+    <h1 style="margin-top:20%; color:#ff4d6d; animation: fade 1.5s;">
+      You are my today, tomorrow & always ❤️<br>
+      Happy Valentine’s Day 💍
+    </h1>
+  `;
+}
