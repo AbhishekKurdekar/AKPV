0500a6b8e1e3c89d9c73d37f343c95c44115ca21
AbhishekKurdekar <95276063+AbhishekKurdekar@users.noreply.github.com>
2026-02-03 19:46:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function unlock() {
   const pass = document.getElementById("password").value;
-  if (pass === "myvalentine") {   // CHANGE PASSWORD HERE
+  if (pass === "04Aug2025") {
     document.getElementById("lockScreen").style.display = "none";
     document.getElementById("content").style.display = "block";
+    document.getElementById("bgMusic").play();
   } else {
     document.getElementById("error").innerText = "Wrong password 💔";
   }
@@ -17,9 +18,26 @@ function moveNo() {
 
 function yesClick() {
   document.body.innerHTML = `
-    <h1 style="margin-top:20%; color:#ff4d6d; animation: fade 1.5s;">
+    <h1 style="margin-top:20%; color:#ff4d6d;">
       You are my today, tomorrow & always ❤️<br>
       Happy Valentine’s Day 💍
     </h1>
   `;
 }
+
+/* Hearts */
+setInterval(() => {
+  const heart = document.createElement("div");
+  heart.className = "heart";
+  heart.innerText = "❤️";
+  heart.style.left = Math.random() * 100 + "vw";
+  heart.style.fontSize = (Math.random() * 20 + 15) + "px";
+  document.body.appendChild(heart);
+  setTimeout(() => heart.remove(), 6000);
+}, 700);
+
+/* Protection */
+document.addEventListener("contextmenu", e => e.preventDefault());
+document.addEventListener("dragstart", e => {
+  if (e.target.tagName === "IMG") e.preventDefault();
+});
